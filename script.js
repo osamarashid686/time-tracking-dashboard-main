fc57325b72810492bd1d4222639e44e18ab3b016
osama <osamarashid686@gmail.com>
2021-11-04 01:55:29 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,81 @@
+let menulink1 = document.getElementById("mlink1");
+let menulink2 = document.getElementById("mlink2");
+let menulink3 = document.getElementById("mlink3");
+
+let workhr = document.getElementById("workhr");
+let worklast = document.getElementById("worklast");
+
+let playhr = document.getElementById("playhr");
+let playlast = document.getElementById("playlast");
+
+let studyhr = document.getElementById("studyhr");
+let studylast = document.getElementById("studylast");
+
+let exercisehr = document.getElementById("exercisehr");
+let exerciselast = document.getElementById("exerciselast");
+
+let socialhr = document.getElementById("socialhr");
+let sociallast = document.getElementById("sociallast");
+
+let selfcarehr = document.getElementById("self-carehr");
+let selfcarelast = document.getElementById("self-carelast");
+
+function chd() {
+    workhr.innerHTML = "5hrs";
+    worklast.innerHTML = "Last Week-7hrs";
+
+    playhr.innerHTML = "1hrs";
+    playlast.innerHTML = "Last Week-2hrs";
+
+    studyhr.innerHTML = "0hrs";
+    studylast.innerHTML = "Last Week-1hrs";
+
+    exercisehr.innerHTML = "1hrs";
+    exerciselast.innerHTML = "Last Week-1hrs";
+
+    socialhr.innerHTML = "1hrs";
+    sociallast.innerHTML = "Last Week-3hrs";
+
+    selfcarehr.innerHTML = "0hrs";
+    selfcarelast.innerHTML = "Last Week-1hrs";
+}
+function chw() {
+    workhr.innerHTML = "32hrs";
+    worklast.innerHTML = "Last Week-36hrs";
+
+    playhr.innerHTML = "10hrs";
+    playlast.innerHTML = "Last Week-8hrs";
+
+    studyhr.innerHTML = "4hrs";
+    studylast.innerHTML = "Last Week-7hrs";
+
+    exercisehr.innerHTML = "5hrs";
+    exerciselast.innerHTML = "Last Week-3hrs";
+
+    socialhr.innerHTML = "8hrs";
+    sociallast.innerHTML = "Last Week-12hrs";
+
+    selfcarehr.innerHTML = "3hrs";
+    selfcarelast.innerHTML = "Last Week-8hrs";
+}
+function chm() {
+    workhr.innerHTML = "103hrs";
+    worklast.innerHTML = "Last Week-128hrs";
+
+    playhr.innerHTML = "100hrs";
+    playlast.innerHTML = "Last Week-220hrs";
+
+    studyhr.innerHTML = "80hrs";
+    studylast.innerHTML = "Last Week-240hrs";
+
+    exercisehr.innerHTML = "267hrs";
+    exerciselast.innerHTML = "Last Week-432hrs";
+
+    socialhr.innerHTML = "189hrs";
+    sociallast.innerHTML = "Last Week-20
+    
+    6hrs";
+
+    selfcarehr.innerHTML = "143hrs";
+    selfcarelast.innerHTML = "Last Week-123hrs";
+}
---
+++ style-guide.md
@@ -0,0 +1,84 @@
+# Front-end Style Guide
+
+## Layout
+
+The designs were created to the following widths:
+
+- Mobile: 375px
+- Desktop: 1440px
+
+## Colors
+
+### Primary
+
+
+### Neutral
+
+
+
+## Typography
+
+### Body Copy
+
+- Font size: 18px (card titles e.g. Work, Play)
+
+### Font
+
+- Family: [Rubik](https://fonts.google.com/specimen/Rubik)
+- Weights: 300, 400, 500
+
+
+  Report for
+  Jeremy Robson
+
+  Daily
+  Weekly
+  Monthly
+
+  Work
+  5hrs <!-- daily -->
+  Previous - 7hrs <!-- daily -->
+  32hrs <!-- weekly -->
+  Previous - 36hrs <!-- weekly -->
+  103hrs <!-- monthly -->
+  Previous - 128hrs <!-- monthly -->
+
+  Play
+  1hr <!-- daily -->
+  Previous - 2hrs <!-- daily -->
+  10hrs <!-- weekly -->
+  Previous - 8hrs <!-- weekly -->
+  23hrs <!-- monthly -->
+  Previous - 29hrs <!-- monthly -->
+
+  Study
+  0hrs <!-- daily -->
+  Previous - 1hr <!-- daily -->
+  4hrs <!-- weekly -->
+  Previous - 7hrs <!-- weekly -->
+  13hrs <!-- monthly -->
+  Previous - 19hrs <!-- monthly -->
+
+  Exercise
+  1hr <!-- daily -->
+  Previous - 1hr <!-- daily -->
+  4hrs <!-- weekly -->
+  Previous - 5hrs <!-- weekly -->
+  11hrs <!-- monthly -->
+  Previous - 18hrs <!-- monthly -->
+
+  Social
+  1hr <!-- daily -->
+  Previous - 3hrs <!-- daily -->
+  5hrs <!-- weekly -->
+  Previous - 10hrs <!-- weekly -->
+  21hrs <!-- monthly -->
+  Previous - 23hrs <!-- monthly -->
+
+  Self Care
+  0hrs <!-- daily -->
+  Previous - 1hr <!-- daily -->
+  2hrs <!-- weekly -->
+  Previous - 2hrs <!-- weekly -->
+  7hrs <!-- monthly -->
+  Previous - 11hrs <!-- monthly -->
